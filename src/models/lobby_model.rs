dbea4ae12147f3060a71acf23f7605cb4b724d7c
YanniAlshoufiHTL <114535252+YanniAlshoufiHTL@users.noreply.github.com>
2023-09-18 21:06:56 +0200

    Modified what's stored in the lobbies

--- src/models/lobby_model.rs
+++ src/models/lobby_model.rs
@@ -1,15 +1,57 @@
 use std::str::FromStr;
 
-use exaworld_dependencies::{DatabaseError, LOBBY_SEPARATOR, LOBBY_PLAYERS_IDS_SEPARATOR};
+use exaworld_dependencies::{DatabaseError, LOBBY_PLAYER_MODELS_SEPARATOR, LOBBY_SEPARATOR};
 use serde::{Deserialize, Serialize};
 
-use crate::{WorldModel, DbModel};
+use crate::{DbModel, PlayerModel, WorldModel};
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct LobbyPlayerModel {
+    pub id: u64,
+    pub model: PlayerModel,
+}
+
+impl ToString for LobbyPlayerModel {
+    fn to_string(&self) -> String {
+        format!(
+            "{}{}{}",
+            self.id,
+            LOBBY_PLAYER_MODELS_SEPARATOR,
+            self.model.to_string()
+        )
+    }
+}
+
+impl FromStr for LobbyPlayerModel {
+    type Err = DatabaseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let split_string: Vec<&str> = s.split(LOBBY_PLAYER_MODELS_SEPARATOR).collect();
+
+        if split_string.len() != 2 {
+            return Err(DatabaseError::EntryCouldNotBeParsed(format!(
+                "The entry ('{}') could not be parsed while trying to get a player model for the lobby. The number of fields isn't correct.",
+                s
+            )));
+        }
+
+        let Ok(id) = split_string[0].parse::<u64>() else {
+            return Err(DatabaseError::EntryCouldNotBeParsed(format!("The entry ('{}') could not be parsed while trying to get the id of the player model for the lobby.", s)));
+        };
+
+        let Ok(model) = split_string[1].parse::<PlayerModel>() else {
+            return Err(DatabaseError::EntryCouldNotBeParsed(format!("The entry ('{}') could not be parsed while trying to parse the player model for a player model entry for the lobby.", s)));
+        };
+
+        Ok(Self { id, model })
+    }
+}
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct LobbyModel {
     pub name: String,
     pub world: WorldModel,
-    pub players_ids: Vec<u64>,
+    pub players: Vec<LobbyPlayerModel>,
 }
 
 impl FromStr for LobbyModel {
@@ -28,26 +70,26 @@ impl FromStr for LobbyModel {
 
         let name = split_string[0].to_string();
 
-        let mut players_ids: Vec<u64> = vec![];
+        let mut players: Vec<LobbyPlayerModel> = vec![];
 
-        for player_id in split_string[2]
-            .split(LOBBY_PLAYERS_IDS_SEPARATOR)
+        for player in split_string[2]
+            .split(LOBBY_PLAYER_MODELS_SEPARATOR)
             .map(|x| x.trim())
             .collect::<Vec<&str>>()
         {
-            if !player_id.is_empty() {
-                let Ok(value) = player_id.parse::<u64>() else {
-                    return Err(DatabaseError::EntryCouldNotBeParsed(format!("Could not construct a lobby from '{}' because a player's id ('{}') could not be parsed.", string, player_id)))
+            if !player.is_empty() {
+                let Ok(value) = player.parse::<LobbyPlayerModel>() else {
+                    return Err(DatabaseError::EntryCouldNotBeParsed(format!("Could not construct a lobby from '{}' because a player's id ('{}') could not be parsed.", string, player)))
                 };
 
-                players_ids.push(value);
+                players.push(value);
             }
         }
 
         Ok(Self {
             name,
             world,
-            players_ids,
+            players,
         })
     }
 }
@@ -60,11 +102,11 @@ impl ToString for LobbyModel {
             LOBBY_SEPARATOR,
             self.world.to_string(),
             LOBBY_SEPARATOR,
-            self.players_ids
+            self.players
                 .iter()
                 .map(|x| x.to_string())
                 .collect::<Vec<String>>()
-                .join(LOBBY_PLAYERS_IDS_SEPARATOR)
+                .join(LOBBY_PLAYER_MODELS_SEPARATOR)
                 .to_string()
         )
     }
