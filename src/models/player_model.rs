a0fd8d22a1f7315e32fb1e30b21d157e165fe58e
YanniAlshoufiHTL <114535252+YanniAlshoufiHTL@users.noreply.github.com>
2023-09-18 21:35:17 +0200

    Added objects the player interacted with as a filed for player model.

--- src/models/player_model.rs
+++ src/models/player_model.rs
@@ -1,9 +1,11 @@
 use std::str::FromStr;
 
-use exaworld_dependencies::{Rgba, Vector, Rotation, PLAYER_SEPARATOR};
+use exaworld_dependencies::{
+    Rgba, Rotation, Vector, OBJECT_PLAYER_INTERACTED_WITH_SEPARATOR, PLAYER_SEPARATOR,
+};
 use serde::{Deserialize, Serialize};
 
-use crate::DbModel;
+use crate::{DbModel, MoodleObjectModel};
 
 #[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct PlayerStyle {
@@ -33,6 +35,7 @@ pub struct PlayerModel {
     pub style: PlayerStyle,
     pub username: String,
     pub lobby_id: Option<u64>,
+    pub objects_interacted_with: Vec<MoodleObjectModel>,
 }
 
 impl FromStr for PlayerModel {
@@ -41,7 +44,7 @@ impl FromStr for PlayerModel {
     fn from_str(string: &str) -> Result<Self, Self::Err> {
         let split_string: Vec<&str> = string.split(PLAYER_SEPARATOR).collect();
 
-        if split_string.len() != 5 {
+        if split_string.len() != 6 {
             return Err(format!("Incorrect number of fields. ('{}')", string).into());
         }
 
@@ -55,12 +58,29 @@ impl FromStr for PlayerModel {
         let pos_z: f32 = split_pos_string[2].parse()?;
         let pos_y: f32 = split_pos_string[1].parse()?;
 
+        let objects_player_interacted_with_string = split_string[5];
+
+        let objectes_player_collected_with_strings: Vec<&str> =
+            objects_player_interacted_with_string
+                .split(OBJECT_PLAYER_INTERACTED_WITH_SEPARATOR)
+                .collect();
+
+        let mut objects: Vec<MoodleObjectModel> = vec![];
+        for object_str in objectes_player_collected_with_strings {
+            let Ok(object) = object_str.parse::<MoodleObjectModel>() else {
+                return Err(format!("Moodle object model could not be parsed. ('{}')", string).into());
+            };
+
+            objects.push(object);
+        }
+
         Ok(Self {
             username: split_string[0].to_string(),
             position: Vector::new(pos_x, pos_y, pos_z),
             direction: Rotation::new(split_string[2].parse()?),
             style: PlayerStyle::from_str(split_string[3])?,
             lobby_id: split_string[4].trim().parse::<u64>().ok(),
+            objects_interacted_with: objects,
         })
     }
 }
@@ -68,7 +88,7 @@ impl FromStr for PlayerModel {
 impl ToString for PlayerModel {
     fn to_string(&self) -> String {
         format!(
-            "{}{}{}:{}:{}{}{}{}{}{}{}",
+            "{}{}{}:{}:{}{}{}{}{}{}{}{}{}",
             self.username,
             PLAYER_SEPARATOR,
             self.position.x,
@@ -83,6 +103,12 @@ impl ToString for PlayerModel {
                 Some(val) => val.to_string(),
                 None => "".to_string(),
             },
+            PLAYER_SEPARATOR,
+            self.objects_interacted_with
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>()
+                .join(OBJECT_PLAYER_INTERACTED_WITH_SEPARATOR)
         )
     }
 }
